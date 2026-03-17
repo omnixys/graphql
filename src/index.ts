2e818c7bf8be866786b3dca558e2c4e47df19b28
Caleb-Script <caleb-script@outlook.de>
2026-03-17 22:20:26 +0100

    fix(graphql): unify enum registration via @omnixys/graphql to avoid type resolution errors

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 export * from "./enums/user.enum.js";
 export * from "./inputs/index.js";
+
+export {
+  ContactOptionsType,
+  GenderType,
+  InterestCategoryType,
+  InterestType,
+  MaritalStatusType,
+  PersonStatusType,
+  PhoneNumberType,
+  RealmRoleType,
+  RelationshipType,
+  StatusType,
+  UserType,
+} from "@omnixys/contracts";
